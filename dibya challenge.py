dae050da4ba1bc19e8b903589bf438a112bea6b4
Dibyajyoti rath <dibyajyotirath2@gmail.com>
2021-11-11 10:49:00 +0530

    todays demo projects

---
+++ dibya challenge.py
@@ -0,0 +1,19 @@
+while True:
+    a=input("enter a string:")
+    for i in a:
+        if i==("1","2","3","4","5") in a:
+            print("not allowed")
+    
+
+        if len(a[0:])>4:
+            print("congratulatin u got the result")
+            print("the result is:",a[4:])
+            break
+        elif len(a[0:])==4:
+            print("the word has only 4 letters\nso u r not going to get any result")
+        
+        
+
+        if len(a[0:])<4:
+            print("sorry it has less then 4 letters\nplz think a big number")
+
---
+++ dibya demo program.py
@@ -0,0 +1,39 @@
+class man:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def nature(self):
+        print(self.name,"is a good guy")
+        print(self.name,"'s age is",self.age)
+class employee(man):
+    def __init__(self,name,age,enum,esal,ecom):
+        super().__init__(name,age)
+        self.enum=enum
+        self.esal=esal
+        self.ecom=ecom
+    def work(self):
+        super().nature()
+        print("employee number is:",self.enum)
+        print("employee salary is:",self.esal)
+        print("company name :",self.ecom)
+class son(man,employee):
+    def __init__(self,name,age,cast,fname,mname):
+        man.__init__(name,age)
+        self.cast=cast
+        self.fname=fname
+    def sonbio(self):
+        .nature()
+        employee.work()
+        print("cast is :",self.cast)
+        print("father's name:",self.fname)
+        print("mother's name:",self.mname)
+
+s=son("dibya",22,"general","manamohan rath","anima rath")
+s.sonbio()
+
+
+
+
+
+        
+
